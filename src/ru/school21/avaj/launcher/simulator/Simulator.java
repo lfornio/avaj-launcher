0c5d7387f6f2f8b0873827bbd595da272abbd7a4
Elena Temyach <ea.lfornio@gmail.com>
2022-11-06 11:49:28 +0300

    регистрация лет.аппаратов

--- src/ru/school21/avaj/launcher/simulator/Simulator.java
+++ src/ru/school21/avaj/launcher/simulator/Simulator.java
@@ -1,8 +1,9 @@
 package ru.school21.avaj.launcher.simulator;
 
-import ru.school21.avaj.launcher.exceptions.*;
+import ru.school21.avaj.launcher.simulator.exceptions.*;
 import ru.school21.avaj.launcher.simulator.aircrafts.AircraftFactory;
 import ru.school21.avaj.launcher.simulator.aircrafts.Flyable;
+import ru.school21.avaj.launcher.simulator.weather.WeatherTower;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -22,19 +23,12 @@ public class Simulator {
             }
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
             int countSimulations = getCountSimulationsFromFile(reader.readLine());
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                Flyable flyable = getFlyablesFromFile(line);
-                aircrafts.add(flyable);
-            }
-            if (aircrafts.isEmpty()) {
-                throw new CountAircraftsError();
-            }
-
+            receiveFlyables(reader);
+            WeatherTower weatherTower = new WeatherTower();
             for (Flyable flyable : aircrafts) {
-                System.out.println(flyable.toString());
+                flyable.registerTower(weatherTower);
             }
+
 
         } catch (ArgumentError | ErrorTypeAircraft | FileError | CountAircraftsError | CountSimulationsError ex) {
             System.out.println(ex.getMessage());
@@ -50,6 +44,28 @@ public class Simulator {
         }
     }
 
+    private static void receiveFlyables(BufferedReader reader) throws IOException {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            Flyable flyable = getFlyableFromFile(line);
+            aircrafts.add(flyable);
+        }
+        if (aircrafts.isEmpty()) {
+            throw new CountAircraftsError();
+        }
+        aircraftsHasDublicat();
+
+    }
+    private static void aircraftsHasDublicat() {
+        for (int i = 0; i < aircrafts.size() - 1; i++) {
+            if(aircrafts.get(i).equals(aircrafts.get(i + 1))) {
+                System.out.println("Дубликат");
+            }
+        }
+        System.out.println("Нет дубликата");
+
+    }
+
     private static int getCountSimulationsFromFile(String line) throws FileError, CountSimulationsError {
         int countSimulations;
         try {
@@ -63,18 +79,18 @@ public class Simulator {
         return countSimulations;
     }
 
-    private static Flyable getFlyablesFromFile(String line) throws FileError, NumberFormatException, ErrorTypeAircraft {
+    private static Flyable getFlyableFromFile(String line) throws FileError, NumberFormatException, ErrorTypeAircraft {
         String[] array = line.split(" ");
         if (array.length != COUNT_INFO_FROM_FILE) {
             throw new FileError();
         }
 
         Flyable flyable = AircraftFactory.newAircraft(
-                line.split(" ")[0],
-                line.split(" ")[1],
-                Integer.parseInt(line.split(" ")[2]),
-                Integer.parseInt(line.split(" ")[3]),
-                Integer.parseInt(line.split(" ")[4])
+                array[0],
+                array[1],
+                Integer.parseInt(array[2]),
+                Integer.parseInt(array[3]),
+                Integer.parseInt(array[4])
         );
         return flyable;
     }
